1529f6a295b5b318aa86d7385c76e4d8fa5c8a8f
crisX666 <144061854+crisX666@users.noreply.github.com>
2024-01-23 22:22:24 -0600

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,23 @@
+//Archivo principal, se separa el código en clase calcButton y calculator_view (funciones de botones y la parte grafica)
+import 'package:chryso_calculator/calculator_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const CalculatorView(),
+    );
+  }
+}
